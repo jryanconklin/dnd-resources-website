ef904f5975acf6927e356d637698ce71a9a1197c
J. Ryan Conklin <jryanconklin@gmail.com>
2016-12-21 14:14:38 -0800

    Finalize Header and Nav Design

--- all/themes/drowsy/templates/page.tpl.php
+++ all/themes/drowsy/templates/page.tpl.php
@@ -1,21 +1,31 @@
 
-<div class="main-header container-fluid">
 
+
+
+<div class="logo-header jumbotron">
   <div class="row">
-
-    <div class="logo-div col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+    <div class="logo-div">
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img class="img-responsive img-thumbnail" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>">
+          <img class="col-sm-6 col-sm-offset-3 img-responsive img-thumbnail" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>">
         </a>
       <?php endif; ?>
     </div> <!-- End logo-div -->
+  </div>
+</div>
 
 
-    <div class="drowsy-user-menu col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+<div id="header" class="main-header container-fluid">
 
+  <div class="row">
+    <div class="drowsy-main-menu col-xs-12 col-sm-6 col-md-6">
+      <?php print render($page['main_menu']); ?>
+    </div> <!-- End drowsy-main-menu -->
+
+
+    <div class="drowsy-user-menu col-xs-12 col-sm-6 col-md-6">
       <?php if ($logged_in): ?>
-          <?php print render($page['user_menu']); ?>
+        <?php print render($page['user_menu']); ?>
       <?php else: ?>
         <ul>
           <li class="menu"><a href="<?php base_path(); ?>user/login">Log In</a></li>
@@ -23,16 +33,29 @@
       <?php endif; ?>
     </div> <!-- End drowsy-user-menu -->
 
-    <div class="drowsy-main-menu">
-      <?php print render($page['main_menu']); ?>
-    </div> <!-- End drowsy-main-menu  -->
+  </div>
 
-  </div> <!-- End Row  -->
-</div> <!-- End main-header -->
+</div> <!-- End id="header" main-header and fluid container -->
 
 
-<div class="container">
-  <?php print render($page['content']); ?>
+  <div class="container"> <!-- Main Body Container -->
+      <!-- Drupal Messages -->
+      <div class="col-lg-12">
+        <?php if ($messages): ?>
+          <div id="messages">
+            <div class="section clearfix">
+              <?php print $messages; ?>
+            </div>
+          </div>
+        <?php endif; ?>
+        <!-- End Drupal Messages -->
 
+      <div class="col-xs-12 col-sm-12 col-md-8">
+        <?php print render($page['content_main']); ?>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-4">
+        <?php print render($page['content_second']); ?>
+      </div>
+    </div>
 
-</div>
+  </div> <!-- End Main Body Container -->
