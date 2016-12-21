b22413423929a7cbd14c1b426a663a9d7ce720b3
J. Ryan Conklin <jryanconklin@gmail.com>
2016-12-21 08:04:41 -0800

    Update Header Styling

--- all/themes/drowsy/templates/page.tpl.php
+++ all/themes/drowsy/templates/page.tpl.php
@@ -3,18 +3,19 @@
 
   <div class="row">
 
-    <div class="logo-div col-sm-4">
+    <div class="logo-div col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img class="img-responsive" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>">
+          <img class="img-responsive img-thumbnail" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>">
         </a>
       <?php endif; ?>
     </div> <!-- End logo-div -->
 
 
-    <div class="drowsy-user-menu col-sm-4">
+    <div class="drowsy-user-menu col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+
       <?php if ($logged_in): ?>
-        <?php print render($page['user_menu']); ?>
+          <?php print render($page['user_menu']); ?>
       <?php else: ?>
         <ul>
           <li class="menu"><a href="<?php base_path(); ?>user/login">Log In</a></li>
@@ -32,4 +33,6 @@
 
 <div class="container">
   <?php print render($page['content']); ?>
+
+
 </div>
